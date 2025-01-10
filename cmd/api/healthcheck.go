8a0a531cefdcfebb1bf796e6e1cbb1b6096ff83e
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-10 21:41:58 +0100

    Updated helper method to send an indented JSON with an envelope

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,15 +7,17 @@ import (
 // healthcheckHandler is the handler that shows information about the application.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Define data map to be converted in JSON.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	// Convert data in JSON with the specified status code using an helper method and
 	// checks for errors.
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,27 +9,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// Retrieve the "id" URL parameter from the current request context, then convert it to
-// an integer and return it. If the operation isn't successful, return 0 and an error.
-func (app *application) readIDParam(r *http.Request) (int64, error) {
-	// Retrieve parameters from URL.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// Get ID.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
-	}
-
-	return id, nil
-}
+// Define an envelope type.
+type envelope map[string]any
 
 // Define a writeJSON() helper for sending responses. This takes the destination
 // http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
 // header map containing any additional HTTP headers we want to include in the response.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
-	js, err := json.Marshal(data)
+	// Use the json.MarshalIndent() function so that whitespace is added to the encoded JSON.
+	// Here we use no line prefix ("") and tab indents ("\t") for each element.
+	// MashalIndent is quite slower compared to Marshal. For resource-constrained application
+	// use json.Marshal instead.
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
@@ -49,4 +41,19 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	w.Write(js)
 
 	return nil
+}
+
+// Retrieve the "id" URL parameter from the current request context, then convert it to
+// an integer and return it. If the operation isn't successful, return 0 and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	// Retrieve parameters from URL.
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// Get ID.
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
 }
