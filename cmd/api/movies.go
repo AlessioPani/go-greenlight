7597d194731fecff4d4bb0c1a3f51c58580ca310
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-10 15:20:29 +0100

    Added helper method to get ID parameter from a request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createMovieHandler is the handler that creates a movie.
@@ -17,12 +14,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // showMovieHandler is the handler that shows a movie by its ID.
 // Method: GET
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// Retrieve parameters from URL.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// Get ID.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
