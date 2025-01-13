6ac483ff668e9f379d2dfdbee53eef09b7167088
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-13 14:10:19 +0100

    Created UnmarshalJSON method for the Runtime type and updated createMovieHandler

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,13 +1,19 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // Declare a custom Runtime type, which has the underlying type int32 (the same as our
 // Movie struct field).
 type Runtime int32
+
+// Define an error that our UnmarshalJSON() method can return if we're unable to parse
+// or convert the JSON string successfully.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Implement a MarshalJSON() method on the Runtime type so that it satisfies the
 // json.Marshaler interface. This should return the JSON-encoded value for the movie
@@ -22,4 +28,39 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	// Convert the quoted string value to a byte slice and return it.
 	return []byte(quotedJSONValue), nil
+}
+
+// Implement a UnmarshalJSON() method on the Runtime type so that it satisfies the
+// json.Unmarshaler interface.
+// IMPORTANT: Because UnmarshalJSON() needs to modify the receiver (our Runtime type),
+// we must use a pointer receiver for this to work correctly.
+// Otherwise, we will only be modifying a copy (which is then discarded when this method returns).
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// We expect to receive a field like "<runtime> mins", so first of all
+	// we remove the quotes "".
+	// If no quotes are available, we raise an invalid runtime format error.
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the input to separate the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Sanitize input by checking its format.
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Parse the number.
+	num, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// If the input is in the valid format, assign
+	// the number to the receiver.
+	*r = Runtime(num)
+
+	return nil
 }
