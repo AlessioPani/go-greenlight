4d55ede66ef64049253033fa6e451092d117b8f1
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-14 21:18:08 +0100

    Improved db init.sh and updated docker-compose.yml

--- db-data/init.sh
+++ db-data/init.sh
@@ -3,11 +3,6 @@
 set -e
 
 # Database setup
-echo "Creating greenlight database as superuser..."
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-	CREATE DATABASE greenlight;
-EOSQL
-
 echo "Opening new database as superuser"
 echo "Adding extension and creating a normal operational user"
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "greenlight" <<-EOSQL
