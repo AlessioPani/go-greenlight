ac10d96ba34b80f682bc0574eae9b8cb98d692ea
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-17 20:59:38 +0100

    added check of X-Expected-Version header to help preventing data race condition

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/AlessioPani/go-greenlight/internal/data"
 	"github.com/AlessioPani/go-greenlight/internal/validator"
@@ -118,6 +119,15 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 			app.serverErrorResponse(w, r, err)
 		}
 		return
+	}
+
+	// If the request contains a X-Expected-Version header, verify that the movie
+	// version in the database matches the expected version specified in the header.
+	if r.Header.Get("X-Expected-Version") != "" {
+		if strconv.Itoa(int(movie.Version)) != r.Header.Get("X-Expected-Version") {
+			app.editConflictResponse(w, r)
+			return
+		}
 	}
 
 	// Declare an input struct to hold the expected data from the client.
