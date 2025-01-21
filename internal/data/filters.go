2b4003ceac134384433854ea9cad4767f9bd922e
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-21 19:55:24 +0100

    added a metadata struct to populate the pagination metadata in the response of a getAll request

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -52,3 +52,28 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
+
+// Metadata is a struct that contains pagination metadata.
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+// calculateMetadata is a function that calculates the pagination metadata,
+// given the total number of records, current page and the page size.
+func calculateMetadata(totalRecords int, page int, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
