21255737c800ca6c27a07908bf7daef35d28b91d
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-01-26 21:04:58 +0100

    Added token generation on the registration handler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/AlessioPani/go-greenlight/internal/data"
 	"github.com/AlessioPani/go-greenlight/internal/validator"
@@ -62,9 +63,21 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// Generate a token for the new user.
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	// Send email to the new user from a new goroutine that runs in background.
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.serverErrorResponse(w, r, err)
 			return
